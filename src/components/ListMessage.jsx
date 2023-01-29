ac4573e1eb80e7139475e0ef8bb34e0ccd24c1aa
SRG <itsmesrg@gmail.com>
2023-01-29 18:57:48 +0530

    List Messages implemented : Shubham

--- src/components/ListMessage.jsx
+++ src/components/ListMessage.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+// import { MessageResponse } from "@frequency-chain/api-augment/interfaces";
 
-function ListMessage() {
+function ListMessage(props) {
+  const listMessages = async () => {
+    const messages = await fetchMessagesForSchema(
+      parseInt(props.schema.schema_id)
+    );
+
+    let allMessages = messages.map((msg, index) => {
+      return {
+        key: index,
+        payload: staticSchema.fromBuffer(
+          Buffer.from(msg.payload.unwrap().buffer)
+        ),
+      };
+    });
+
+    setListOfMessage(allMessages);
+  }
+  useEffect(() => {
+    fetchData();
+    setLoading(false);
+}, [props.schema.schema_id])
   return (
-    <div>listMessage</div>
+    <>
+      <Space direction="vertical">
+        <Button onClick={listMessages}>Refresh Messages</Button>
+        <Table dataSource={listOfMessages} size="small">
+          <Column
+            title="Messages"
+            dataIndex="payload"
+            render={(msg) => (
+              <pre>{JSON.stringify({ msg }, null, 2)}</pre>
+            )}
+          />
+        </Table>
+      </Space>
+    </>
   )
 }
 
