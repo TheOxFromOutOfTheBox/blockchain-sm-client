0eff8b44db8168e18e5b454eba8b424d0c93fe29
SRG <itsmesrg@gmail.com>
2023-02-02 21:47:31 +0530

    added code for reference in create message

--- src/components/CreateMessage.jsx
+++ src/components/CreateMessage.jsx
@@ -8,6 +8,21 @@ import * as avro from 'avsc'
 
 
 
+// import { testCompression, testParquetSchema } from "../helpers/parquet";
+// import * as generators from "@dsnp/test-generators";
+// import broadcastSchema from "./broadcast";
+
+// describe("Broadcast Spec", () => {
+//   testParquetSchema(broadcastSchema);
+
+//   testCompression("broadcast", broadcastSchema, () => ({
+//     announcementType: 2,
+//     contentHash: generators.generateHash(),
+//     fromId: generators.randInt(10000000),
+//     url: `https://www.imadapp.com/data/posts/${generators.generateHash()}`,
+//   }));
+// });
+
 const Dropdown = (props) => {
     const items = props.items
     const [showDropdown, setShowDropdown] = useState(false);
