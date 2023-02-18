d3b90b405df88fc89cbdf149854b4794630eaacb
SRG <itsmesrg@gmail.com>
2023-02-18 19:46:39 +0530

    fetch message working

--- src/components/ListMessage.jsx
+++ src/components/ListMessage.jsx
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from 'react'
+import { fetchMessagesForSchema } from '../services/chain/apis/extrinsic';
+import { Schema } from './CreateMessage';
 
 function ListMessage(props) {
     const [listOfMessage,setListOfMessage]=useState([])
     const listMessages = async () => {
         const messages = await fetchMessagesForSchema(
-            parseInt(props.schema.schema_id)
+            // parseInt(props.schema.schema_id)
+            2
         );
 
         let allMessages = messages.map((msg, index) => {
             return {
                 key: index,
-                payload: staticSchema.fromBuffer(
+                payload: Schema.fromBuffer(
                     Buffer.from(msg.payload.unwrap().buffer)
                 ),
             };
         });
 
         setListOfMessage(allMessages);
+        console.log(listOfMessage)
     }
-    console.log(allMessages)
     return (
         <>
             <p>Hi we here now.</p>
+            <button onClick={()=>listMessages()} className='bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600'> Fetch Messages</button>
         </>
     )
 }
