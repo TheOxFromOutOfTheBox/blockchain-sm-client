224c7a62d950dcdea172845b5fd6e89f978bf705
SRG <itsmesrg@gmail.com>
2023-03-16 13:25:27 +0530

    commented web3

--- routes/parquetRoutes.js
+++ routes/parquetRoutes.js
@@ -1,47 +1,47 @@
 const router = require('express').Router();
 const parquet = require('@dsnp/parquetjs');
 const fs = require('fs');
-const Web3Storage = require('web3.storage');
+// const Web3Storage = require('web3.storage');
 
 const ParquetSchema = require('../Schema/parquetSchema')
 
 
 
-function getAccessToken () {
-  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDZlNjNCNTE5ZGREMThDYjA4NTgxM0I1Y0JCRTZEQWE3ODU3YjJFZTkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzc1NjczMzQ1NzYsIm5hbWUiOiJTY2hlbWEgVGVzdCJ9.rTSBPglXKf8pwb2qdISq9mFuElLz2YGMB-34YWyOkVc'
-}
+// function getAccessToken () {
+//   return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDZlNjNCNTE5ZGREMThDYjA4NTgxM0I1Y0JCRTZEQWE3ODU3YjJFZTkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzc1NjczMzQ1NzYsIm5hbWUiOiJTY2hlbWEgVGVzdCJ9.rTSBPglXKf8pwb2qdISq9mFuElLz2YGMB-34YWyOkVc'
+// }
 
-function makeStorageClient () {
-  return new Web3Storage.Web3Storage({ token: getAccessToken() })
-}
+// function makeStorageClient () {
+//   return new Web3Storage.Web3Storage({ token: getAccessToken() })
+// }
 
-async function getFiles (path) {
-  console.log(`read from ${path}`);
-  const files = await Web3Storage.getFilesFromPath(path);
-  console.log(`read ${files.length} file(s) from ${path}`);
-  return files;
-}
-async function storeFiles (files) {
-    console.log('wntered');
-    const client = makeStorageClient()
-    const cid = await client.put(files)
-    console.log('stored files with cid:', cid)
-    return cid
-  }
+// async function getFiles (path) {
+//   console.log(`read from ${path}`);
+//   const files = await Web3Storage.getFilesFromPath(path);
+//   console.log(`read ${files.length} file(s) from ${path}`);
+//   return files;
+// }
+// async function storeFiles (files) {
+//     console.log('wntered');
+//     const client = makeStorageClient()
+//     const cid = await client.put(files)
+//     console.log('stored files with cid:', cid)
+//     return cid
+//   }
 
-async function retrieve (cid) {
-  const client = makeStorageClient()
-  const res = await client.get(cid)
-  console.log(`Got a response! [${res.status}] ${res.statusText}`)
-  if (!res.ok) {
-    throw new Error(`failed to get ${cid}`)
-  }
-  const files = await res.files()
-  for (const file1 of files) {
-    console.log(`${file1.pins} -- ${file1.path} -- ${file1.size}`)
-  }
-  // request succeeded! do something with the response object here...
-}
+// async function retrieve (cid) {
+//   const client = makeStorageClient()
+//   const res = await client.get(cid)
+//   console.log(`Got a response! [${res.status}] ${res.statusText}`)
+//   if (!res.ok) {
+//     throw new Error(`failed to get ${cid}`)
+//   }
+//   const files = await res.files()
+//   for (const file1 of files) {
+//     console.log(`${file1.pins} -- ${file1.path} -- ${file1.size}`)
+//   }
+//   // request succeeded! do something with the response object here...
+// }
 
 router.get('/', async (req,res) => {
 
@@ -63,9 +63,9 @@ router.get('/', async (req,res) => {
 
     // let pq_file = fs.readFileSync("./fruits.parquet");
 
-    const file1 = await getFiles('./files/fruits.parquet')
-    console.log(file1)
-    storeFiles(file1);
+    // const file1 = await getFiles('./files/fruits.parquet')
+    // console.log(file1)
+    // storeFiles(file1);
 
     res.send("parquet append successful."); 
 });
